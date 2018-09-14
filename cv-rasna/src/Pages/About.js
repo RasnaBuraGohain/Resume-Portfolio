50a3ac30df42e12144e988d70303c9177ad08ab0
unknown <rasna.maarten@gmail.com>
2018-09-14 15:15:15 +0200

    background image

--- cv-rasna/src/Pages/About.js
+++ cv-rasna/src/Pages/About.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import rasna from './rasna.jpg'
 
 const AboutView = ({ dispatch }) => (
     <main>
-        <h1>A little bit about me</h1>
 
+        <h1>Hello there, I'm Rasna </h1>
+        <div className="profile">
+            <img src={rasna} alt="rasna" ></img>
+
+            <p>With the core interest in <b>Graphic Designing</b> I recently completed a Front-End
+            Development course at Intec-Brussel.
+                A creative and ambitious person with a <b>Bachelors’ Degree</b> in Fashion from India and <b>Advanced Diploma</b> in Fashion
+                from New Zealand.
+            <br />
+                An experienced creative professional with extensive management skills and
+            developed a responsible approach to any task that I undertake.
+                Always eager to learn and overcome any challenges.
+                Seeking a membership in a motivational organization, to prove my progress in
+            the areas entrusted to me and always waiting to secure challenging issues.</p>
+        </div>
 
     </main>
 )
--- cv-rasna/src/Pages/Home.js
+++ cv-rasna/src/Pages/Home.js
@@ -1,25 +1,12 @@
 import React from 'react'
-import rasna from './rasna.jpg'
+
 
 const HomeView = () => (
-    <main>
-        <h1>Hello there, I'm Rasna </h1>
-        <div className="profile">
-            <img src={rasna} alt="rasna" ></img>
+    <div>
+        <h1>Rasna Bura Gohain</h1>
+        <p>Front-End Developer | Graphic Designer | Fashion designer | Stylist</p>
 
-            <p>With the core interest in <b>Graphic Designing</b> I recently completed a Front-End
-            Development course at Intec-Brussel.
-                A creative and ambitious person with a <b>Bachelors’ Degree</b> in Fashion from India and <b>Advanced Diploma</b> in Fashion
-                from New Zealand.
-            <br />
-                An experienced creative professional with extensive management skills and
-            developed a responsible approach to any task that I undertake.
-                Always eager to learn and overcome any challenges.
-                Seeking a membership in a motivational organization, to prove my progress in
-            the areas entrusted to me and always waiting to secure challenging issues.</p>
-        </div>
-
-    </main>
+    </div>
 )
 
 export default HomeView
